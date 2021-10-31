5864e2dbfc675e419bec5180f64e101a8aefb4e2
Miguel Christian Sanchez <mcsanche@ucsd.edu>
2021-10-30 18:18:10 -0700

    explore done

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -44,8 +44,6 @@ async function fetchRecipes() {
 
     let promises = []; // keep track of each promise we make fetching the urls
 
-    recipeData.recipes = [];
-
     recipes.forEach(url => {
       // add each fetch promise to the array
       promises.push(
@@ -66,7 +64,10 @@ async function fetchRecipes() {
           // add the data (we'll process it later) 
           .then(data => {
             //console.log(data);
-            recipeData.recipes.push(data);
+            recipeData[url] = {
+              data: data,
+              card: null
+            };
           })
       );
     });
@@ -94,18 +95,49 @@ function createRecipeCards() {
   // show any others you've added when the user clicks on the "Show more" button.
 
   // Part 1 Expose - TODO
-  console.log(recipeData);
   const main = document.querySelector('main');
-  for (let i = 0; i < recipeData.recipes.length; i++) {
-    let recipeInfo = recipeData.recipes[i];
+  for (let i = 0; i < 3; i++) {
+    let recipeInfo = recipeData[recipes[i]].data;
     const newCard = document.createElement('recipe-card');
     newCard.data = recipeInfo;
+
+    recipeData[recipes[i]].card = newCard;
     main.appendChild(newCard);
   }
 }
 
 function toggleMoreCards(event) {
-  
+  const arrow = document.querySelector('#button-wrapper > img');
+  const button = document.querySelector('button');
+  const main = document.querySelector('main');
+
+  if (button.textContent == "Show more") { 
+    // go through each recipe url
+    recipes.forEach(url => {
+      // if card doesn't exist for the url
+      if (!recipeData[url].card) { 
+        // make a new card
+        const newCard = document.createElement('recipe-card');
+        newCard.data = recipeData[url].data;
+        
+        recipeData[url].card = newCard;
+        main.appendChild(newCard);
+      } else { // if the card already exists
+        // we just unhide it
+        recipeData[url].card.removeAttribute('hidden');
+      }
+    });
+    arrow.hidden = true;
+    button.textContent = "Show less";
+  } else {
+    // hide all but first three
+    for (let i = 3; i < recipes.length; i++) {
+      let url = recipes[i];
+      recipeData[url].card.setAttribute('hidden',true);
+    }
+    arrow.hidden = false;
+    button.textContent = "Show more";
+  }
 }
 
 function bindShowMore() {
@@ -117,5 +149,7 @@ function bindShowMore() {
   // in the recipeData object where you stored them/
 
   // Part 2 Explore - TODO
+  const button = document.getElementById('button-wrapper');
 
+  button.addEventListener('click', toggleMoreCards);
 }
